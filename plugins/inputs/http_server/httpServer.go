dd8e02a9715e4c56c3c220f450b66afe6d56ae4b
wei.ding <wei.ding@mihoyo.com>
2020-08-18 21:19:50 +0800

    fix

--- plugins/inputs/http_server/httpServer.go
+++ plugins/inputs/http_server/httpServer.go
@@ -69,7 +69,7 @@ L:
     			mm := "receive_metrics"
     			for k,_ := range t.tagFields {
     				acc.AddGauge( mm, t.tagFields[k], t.tagTags[k], time.Now() )
-    				fmt.Printf("*********tt****%v***%v\n",t.tagFields[k], t.tagTags[k])
+    				//fmt.Printf("*********tt****%v***%v\n",t.tagFields[k], t.tagTags[k])
 				}
 
 				t.tagFields =  map[string] map[string]interface{}{}
@@ -167,7 +167,7 @@ func (s *HttpServer) Metric(w http.ResponseWriter, r *http.Request, ps httproute
 	if mysqlTag == true {
 		metrics = metrics + "_mysql"
 	}
-	fmt.Printf("check******%v**%v****%v***%v***%v\n",redisTag,mysqlTag,metrics,tags,val)
+	//fmt.Printf("check******%v**%v****%v***%v***%v\n",redisTag,mysqlTag,metrics,tags,val)
 	s.bufCh <- &Point{mm:metrics, tags:tags, fields: val,times:tt,tagS:tagS}
 }
 
